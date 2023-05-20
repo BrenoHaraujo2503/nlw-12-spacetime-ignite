5236dace8df1b3234e795b4de8ea1d7e0bf5f3a4
Breno Henrique <brenoharaujomorais@gmail.com>
2023-05-20 16:54:05 -0300

    Aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
 import 'dotenv/config'
 
 import fastify from 'fastify'
+
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import jwt from '@fastify/jwt'
+
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
@@ -14,9 +19,15 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime',
 })
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
